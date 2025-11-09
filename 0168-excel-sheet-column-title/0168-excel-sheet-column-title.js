5cfc7756ee6dbedb20898b3146406aa43310c510
Abdul Vahab A A <111953542+abdulvahabaa@users.noreply.github.com>
2025-11-09 23:20:09 +0530

    Time: 0 ms (100%), Space: 53.8 MB (58.75%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} columnNumber
+ * @return {string}
+ */
+var convertToTitle = function(columnNumber) {
+     let result = '';
+    while (columnNumber > 0) {
+        columnNumber--;
+        const remainder = columnNumber % 26;
+        const char = String.fromCharCode(65 + remainder);
+        result = char + result;
+        columnNumber = Math.floor(columnNumber / 26);
+
+    }
+    return result;
+ };
